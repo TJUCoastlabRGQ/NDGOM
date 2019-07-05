115468478bc5c608968d33345ee65770e0fe7516
RGQTJUCoastlab <2015205064@tju.edu.cn>
2019-07-05 11:37:10 +0800

    This version is commited to replace the version: Homogeneous Dirichlet boundary condition at the wet-dry interface, zero local flux and zero numerical flux worked

--- NdgPhys/NdgMatSolver/NdgNonhydrostaticSolver/@NdgQuadratureFreeNonhydrostaticSolver1d/matCalculateDownwindedFphysDerivative.m
+++ NdgPhys/NdgMatSolver/NdgNonhydrostaticSolver/@NdgQuadratureFreeNonhydrostaticSolver1d/matCalculateDownwindedFphysDerivative.m
@@ -8,13 +8,13 @@ InnerEdge = mesh.InnerEdge;
 [ fluxX ] = mxEvaluateDownwindNumFlux( mesh.status, InnerEdge.FToE, ...
     fm(:,:,2), fp(:,:,2), vfmx, vfpx, InnerEdge.nx);
 
-% [ fluxMx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, vfmx, InnerEdge.nx );
-% 
-% [ fluxPx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, ...
-%     vfpx, InnerEdge.nx );
+[ fluxMx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, vfmx, InnerEdge.nx );
 
-fluxMx = InnerEdge.nx .* vfmx;
-fluxPx = InnerEdge.nx .* vfpx;
+[ fluxPx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, ...
+    vfpx, InnerEdge.nx );
+
+% fluxMx = InnerEdge.nx .* vfmx;
+% fluxPx = InnerEdge.nx .* vfpx;
 
 % the Inner edge contribution
 DownwindedTermX = InnerEdge.matEvaluateStrongFromEdgeRHS(fluxMx, fluxPx, fluxX);
@@ -31,8 +31,8 @@ vfpx = obj.matImposeNonhydroRelatedBoundaryCondition( vfmx, vfpx, enumNonhydroBo
 [ fluxX ] = mxEvaluateDownwindNumFlux( mesh.status, BoundaryEdge.FToE, ...
     fm(:,:,2), fp(:,:,2), vfmx, vfpx, BoundaryEdge.nx );
 
-% [ fluxMx ] = mxEvaluateSurfFlux( mesh.status, BoundaryEdge.FToE, vfmx, BoundaryEdge.nx );
-fluxMx = BoundaryEdge.nx .* vfmx;
+[ fluxMx ] = mxEvaluateSurfFlux( mesh.status, BoundaryEdge.FToE, vfmx, BoundaryEdge.nx );
+% fluxMx = BoundaryEdge.nx .* vfmx;
 % the boundary edge contribution
 DownwindedTermX = -DownwindedTermX - BoundaryEdge.matEvaluateStrongFromEdgeRHS(fluxMx, fluxX);
 
--- NdgPhys/NdgMatSolver/NdgNonhydrostaticSolver/@NdgQuadratureFreeNonhydrostaticSolver1d/matCalculateUpwindedFphysDerivative.m
+++ NdgPhys/NdgMatSolver/NdgNonhydrostaticSolver/@NdgQuadratureFreeNonhydrostaticSolver1d/matCalculateUpwindedFphysDerivative.m
@@ -8,13 +8,13 @@ InnerEdge = mesh.InnerEdge;
     fm(:,:,2), fp(:,:,2), vfmx, vfpx, InnerEdge.nx );
 
 % % getLocalAndAdjacentFluxTerm
-fluxMx = InnerEdge.nx .* vfmx;
-fluxPx = InnerEdge.nx .* vfpx;
+% fluxMx = InnerEdge.nx .* vfmx;
+% fluxPx = InnerEdge.nx .* vfpx;
 
-% [ fluxMx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, vfmx, InnerEdge.nx );
-% 
-% [ fluxPx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, ...
-%     vfpx, InnerEdge.nx );
+[ fluxMx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, vfmx, InnerEdge.nx );
+
+[ fluxPx ] = mxEvaluateSurfFlux( mesh.status, InnerEdge.FToE, ...
+    vfpx, InnerEdge.nx );
 
 % the Inner edge contribution
 UpwindedTermX = InnerEdge.matEvaluateStrongFromEdgeRHS(fluxMx, fluxPx, fluxX);
@@ -28,8 +28,8 @@ BoundaryEdge = mesh.BoundaryEdge;
 [ fluxX ] = mxEvaluateUpwindNumFlux( mesh.status, BoundaryEdge.FToE, ...
     fm(:,:,2), fp(:,:,2), vfmx, vfpx, BoundaryEdge.nx );
 
-% [ fluxMx ] = mxEvaluateSurfFlux( mesh.status, BoundaryEdge.FToE, vfmx, BoundaryEdge.nx );
-fluxMx = BoundaryEdge.nx .* vfmx;
+[ fluxMx ] = mxEvaluateSurfFlux( mesh.status, BoundaryEdge.FToE, vfmx, BoundaryEdge.nx );
+% fluxMx = BoundaryEdge.nx .* vfmx;
 % the boundary edge contribution
 UpwindedTermX = -UpwindedTermX - BoundaryEdge.matEvaluateStrongFromEdgeRHS(fluxMx, fluxX);
 
